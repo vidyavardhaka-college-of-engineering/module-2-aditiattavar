59484dffa721801383fe6f26548e557e8ad9fec3
aditiattavar <84765968+aditiattavar@users.noreply.github.com>
2021-06-15 10:26:47 +0530

    done

--- Program-10.c
+++ Program-10.c
@@ -1,7 +1,5 @@
 //INPUT - 4 30 20 10 60
 //OUTPUT: The largest and smallest elements are 60 and 10
-
-//INSERT THE MISSING CODE 
 #include <stdio.h>
 
 int main()
@@ -12,7 +10,7 @@ int main()
 
     for (i = 0; i < n; i++)
     {
-        //printf("Enter the value of a[%d] : ",i);
+        printf("Enter the value of a[%d]:  ",i);
         scanf("%d", &arr[i]);
     }
 
@@ -28,13 +26,11 @@ int main()
         
         if (arr[i] >large  )//missing code
         {
-            large= arr[];//missing code
+            large= arr[i];//missing code
         }
     }
 
-    printf("The largest element of the array = %d", large);
-    printf("The smallest element of the array = %d",);
-
+    printf("\nThe largest and smallest elements are %d and %d", large,small);
+    
     return 0;
 }
-
