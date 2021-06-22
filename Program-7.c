32ca4c52239e3e4af4cfe09b253f605cc0eb9721
aditiattavar <84765968+aditiattavar@users.noreply.github.com>
2021-06-22 11:03:53 +0530

    done

--- Program-7.c
+++ Program-7.c
@@ -1,18 +1,19 @@
 //INPUT - 3 1 2 3
 //OUTPUT- 3 2 1
 //INSERT THE MISSING CODE 
+ 
 #include<stdio.h>
 	int main()
 	{
-	  int a[10];
-    int i,n;
+	  int a[10],i,n;
 	  printf("Enter size of the array and the array elements");
 	  scanf("%d",&n);
 	  
-	  for(i=0;i<n;i++)
+	  for(i=0;i<n;i++){
 	  scanf("%d",&a[i]);//missing code
+	  }
 	  printf("Array elements in reverse order : ");
-	  for(i=n;i;i--) //missing code
+	  for(i=n-1;i>=0;i--) //missing code
 	  printf("%d ",a[i]);
 	  return(0);
 	}
