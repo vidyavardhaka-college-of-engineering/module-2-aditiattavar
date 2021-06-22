eabaf0f0df74a6dadd11bab3d376055ab6f1d701
aditiattavar <84765968+aditiattavar@users.noreply.github.com>
2021-06-22 10:56:47 +0530

    done

--- Program-7.c
+++ Program-7.c
@@ -1,6 +1,5 @@
 //INPUT - 3 1 2 3
 //OUTPUT- 3 2 1
-
 //INSERT THE MISSING CODE 
 #include<stdio.h>
 	int main()
@@ -13,7 +12,7 @@
 	  for(i=0;i<n;i++)
 	  scanf("%d",&a[i]);//missing code
 	  printf("Array elements in reverse order : ");
-	  for(i=;i;i) //missing code
+	  for(i=n;i;i--) //missing code
 	  printf("%d ",a[i]);
 	  return(0);
 	}
